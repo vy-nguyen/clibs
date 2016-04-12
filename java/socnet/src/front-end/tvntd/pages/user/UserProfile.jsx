87894fe336a9331be458d69f60cc741e33739a2f
Vy Nguyen <vynguyen@bienhoa>
2016-04-12 00:21:16 -0700

    Wire up UserProfile form.

--- java/socnet/src/front-end/tvntd/pages/user/UserProfile.jsx
+++ java/socnet/src/front-end/tvntd/pages/user/UserProfile.jsx
@@ -7,6 +7,7 @@
 import React              from 'react-mod';
 import Reflux             from 'reflux';
 
+import UiValidate         from 'vntd-shared/forms/validation/UiValidate.jsx';
 import MenuStore          from 'vntd-shared/stores/DropdownMenuStore.jsx';
 import PanelStore         from 'vntd-shared/stores/PanelStore.jsx';
 import TabPanelStore      from 'vntd-shared/stores/TabPanelStore.jsx';
@@ -18,6 +19,8 @@ import ProfileCover       from './ProfileCover.jsx';
 import UserAvatar         from './UserAvatar.jsx';
 
 let UserInfo = React.createClass({
+    mixins: [Reflux.connect(UserStore)],
+
     profileMenu: {
         reactId  : 'profile',
         iconFmt  : 'btn-xs btn-success',
@@ -58,16 +61,62 @@ let UserInfo = React.createClass({
             MenuStore.setDropdownMenu(this.profileMenu.reactId, this.profileMenu);
             PanelStore.setPanel(this.panelData.reactId, this.panelData);
         }
-        return this.panelData;
+        return UserStore.getData();
+    },
+
+    _submitUpdate: function() {
     },
 
     render: function() {
+        let self = this.state.userSelf;
+        if (self == undefined || self == null) {
+            console.log(this.state);
+            return null;
+        }
         return (
-            <Panel reactId={this.panelData.reactId}>
-                <form className="form-horizontal">
+            <Panel reactId={this.panelData.reactId} className="well no-padding">
+                <form className="client-form" onSubmit={this._submitUpdate}>
                     <fieldset>
-                        <legend>Default Form Elements</legend>
-                        <h1>About me</h1>
+                        <section className="row">
+                            <div className="form-group alert alert-danger" id="id-profile-error" style={{display:"none"}}>
+                                <a className="close" data-dismiss="alert" aria-label="close">x</a>
+                                <div id="id-profile-error-text"></div>
+                            </div>
+                        </section>
+                    </fieldset>
+                    <legend>About me</legend>
+                    <fieldset>
+                        <div className="row form-group">
+                            <label className="col-sm-2 col-md-2 col-lg-2 control-label" for="textinput">First Name</label>
+                            <div className="col-sm-10 col-md-10 col-lg-6">
+                                <input type="text" className="form-control" name="firstname" ref="firstName" placeholder={self.firstName}/>
+                            </div>
+                        </div>
+                        <div className="row form-group">
+                            <label className="col-sm-2 col-md-2 col-lg-2 control-label" for="textinput">Last Name</label>
+                            <div className="col-sm-10 col-md-10 col-lg-6">
+                                <input type="text" className="form-control" name="lastname" ref="lastName" placeholder={self.lastName}/>
+                            </div>
+                        </div>
+                    </fieldset>
+                    
+                    <legend>My preferences</legend>
+                    <fieldset>
+                    </fieldset>
+
+                    <legend>My privacy settings</legend>
+                    <fieldset>
+                    </fieldset>
+
+                    <fieldset>
+                        <div className="row form-group">
+                            <div className="col-sm-offset-2 col-sm-10">
+                                <div className="pull-right">
+                                    <button type="submit" className="btn btn-default">Cancel</button>
+                                    <button type="submit" className="btn btn-primary">Save</button>
+                                </div>
+                            </div>
+                        </div>
                     </fieldset>
                 </form>
             </Panel>
