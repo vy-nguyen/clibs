cdefac856707bf0034b77efdfe812de15013ab2c
Vy Nguyen <vynguyen@bienhoa>
2016-05-28 16:58:56 -0700

    Use common KeyValueTable lib to display Author component.

---
+++ java/socnet/src/front-end/shared/layout/KeyValueTable.jsx
@@ -0,0 +1,67 @@
+/**
+ * Copyright by Vy Nguyen (2016)
+ * BSD License
+ */
+'use strict';
+
+import _      from 'lodash';
+import React  from 'react-mod';
+
+let KeyValueTable = React.createClass({
+    render: function() {
+        let oddRowFmt     = this.props.oddRowFmt || "success";
+        let oddRowKeyFmt  = this.props.oddRowKeyFmt || "label label-danger";
+        let oddRowValFmt  = this.props.oddRowValFmt || "";
+
+        let evenRowFmt    = this.props.evenRowFmt || "info";
+        let evenRowKeyFmt = this.props.evenRowKeyFmt || "label label-success";
+        let evenRowValFmt = this.props.evenRowValFmt || "";
+
+        let rowFmt = null;
+        let keyFmt = null;
+        let valFmt = null;
+        let row = [];
+
+        _.forOwn(this.props.keyValueList, function(elm, idx) {
+            if (idx % 2) {
+                rowFmt = oddRowFmt;
+                keyFmt = elm.keyFmt || oddRowKeyFmt;
+                valFmt = elm.valFmt || oddRowValFmt;
+            } else {
+                rowFmt = evenRowFmt;
+                keyFmt = elm.keyFmt || evenRowKeyFmt;
+                valFmt = elm.valFmt || evenRowValFmt;
+            }
+            if (elm.valRows === true) {
+                row.push(
+                    <tr key={_.uniqueId("kv-elm-")} className={rowFmt}>
+                        <td><span className={keyFmt}>{elm.key}</span></td>
+                    </tr>
+                );
+                _.forOwn(elm.val, function(v, index) {
+                    row.push(
+                        <tr key={_.uniqueId("kv-elm-")} className={rowFmt}>
+                            <td colspan="2"><span className={valFmt}>{v}</span></td>
+                        </tr>
+                    )
+                });
+            } else {
+                row.push(
+                    <tr key={_.uniqueId("kv-elm-")} className={rowFmt}>
+                        <td><span className={keyFmt}>{elm.key}</span></td>
+                        <td><span className={valFmt}>{elm.val}</span></td>
+                    </tr>
+                );
+            }
+        });
+        return (
+            <table className={"table " + this.props.className}>
+                <tbody>
+                    {row}
+                </tbody>
+            </table>
+        );
+    }
+});
+
+export default KeyValueTable;
--- java/socnet/src/front-end/tvntd/components/Author.jsx
+++ java/socnet/src/front-end/tvntd/components/Author.jsx
@@ -9,28 +9,28 @@ import React            from 'react-mod';
 import ProfileCover     from './ProfileCover.jsx';
 import { htmlCodes }    from 'vntd-root/config/constants';
 import UserIcon         from 'vntd-root/components/UserIcon.jsx';
+import KeyValueTable    from 'vntd-shared/layout/KeyValueTable.jsx';
 
 let Author = React.createClass({
-
-    renderTopics: function(items) {
-        let out = [];
-        out.push(<br key={_.uniqueId('topic-list-')}/>);
-        _.forEach(items, function(it, idx) {
-            out.push(<span key={_.uniqueId('author-list-')}><span className={it.icon}>{it.tag}</span> {it.text}<br/></span>);
-        });
-        return out;
-    },
 
     renderAboutMe: function(aboutList) {
         if (aboutList == null) {
             return null;
         }
         return aboutList.map(function(item, index) {
+            let topicList = item.topicList.map(function(topic, idx) {
+                return {
+                    key    : topic.tag,
+                    val    : topic.text,
+                    keyFmt : topic.icon,
+                    valRows: topic.text.constructor === Array ? true : false
+                }
+            });
             return (
                 <div key={_.uniqueId('about-list-')}>
                     <hr/>
                     <strong><i className={item.topicIcon}></i>{item.topicText}</strong>
-                    {this.renderTopics(item.topicList)}
+                    <KeyValueTable keyValueList={topicList}/>
                 </div>
             );
         }.bind(this));
@@ -44,6 +44,21 @@ let Author = React.createClass({
         let infoStyle = {
             backgroundImage: 'url(' + this.props.user.coverImg + ')'
         };
+        let moneyKv = [{
+            key: "Earned",
+            val: self.moneyEarned
+        }, {
+            key: "Issued",
+            val: self.moneyIssued
+        }];
+        let creditKv = [{
+            key: "Earned",
+            val: self.creditEarned
+        }, {
+            key: "Issued",
+            val: self.creditIssued
+        }];
+
         return (
             <div className="row">
                 <div className="col-sm-12" style={infoStyle}>
@@ -57,9 +72,9 @@ let Author = React.createClass({
                     <div className="well well-sm" style={{background: "rgba(255,255,255,0.8)"}}>
                         <div className="box-header">
                             <h1 className="profile-username text-center">
-                            {self.firstName} <span className="semi-bold">{self.lastName}</span>
-                            <br/>
-                            <small>{self.userStatus}</small>
+                                {self.firstName} <span className="semi-bold">{self.lastName}</span>
+                                <br/>
+                                <small>{self.userStatus}</small>
                             </h1>
                             <h4 className="font-md"><strong>{self.followers}</strong><small> Followers</small></h4>
                             <h4 className="font-md"><strong>{self.connections}</strong><small> Connections</small></h4>
@@ -68,32 +83,11 @@ let Author = React.createClass({
                             <strong><i className='fa fa-book margin-r-5'></i>Public Transactions</strong>
                             <p/>
                             <span className="label label-success">Money</span>
-                            <table className="table">
-                                <tbody>
-                                    <tr className="info">
-                                        <th><span className='label label-success'>Earned</span></th>
-                                        <th>{self.moneyEarned}</th>
-                                    </tr>
-                                    <tr className="success">
-                                        <th><span className='label label-danger'>Issued</span></th>
-                                        <th>{self.moneyIssued}</th>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            <p/>
+                            <KeyValueTable keyValueList={moneyKv}/>
+
                             <span className="label label-warning">Credit</span>
-                            <table className="table">
-                                <tbody>
-                                    <tr className="info">
-                                        <th><span className='label label-success'>Earned</span></th>
-                                        <th>{self.creditEarned}</th>
-                                    </tr>
-                                    <tr className="success">
-                                        <th><span className='label label-danger'>Issued</span></th>
-                                        <th>{self.creditIssued}</th>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            <KeyValueTable keyValueList={creditKv}/>
+
                             {this.renderAboutMe(this.props.user.aboutList)}
                         </div>
                     </div>
