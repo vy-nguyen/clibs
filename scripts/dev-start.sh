0ff737c8dd49da2ad255c09da6019b9a9eb4cab1
Vy Nguyen <vynguyen@bienhoa>
2017-01-13 21:33:01 -0800

    Update dev-start script. Change spring logging to info level.

--- scripts/dev-start.sh
+++ scripts/dev-start.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Install gradle stuffs
+#
+sudo apt-get install software-properties-common python-software-properties
+sudo add-apt-repository ppa:cwchien/gradle
+sudo add-apt-repository ppa:webupd8team/java -y
+
+sudo apt-get update
+sudo apt-get install gradle
+
+# Install java
+#
+sudo apt-get install oracle-java8-set-default
+
+# Install nodejs, npm...
+#
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install npm -g
+sudo npm install bower -g
+npm install webpack
+
 # Create links...
 #
 pushd .
@@ -11,6 +33,12 @@ popd
 
 # Create mysql database & account.
 #
+sudo apt-get install mysql-server
+
+#
+# mysql --user=root -p
+# GRANT ALL PRIVILEGES ON *.* TO 'socnet'@'localhost';
+# FLUSH PRIVILEGES;
 
 # Sync front-end modules.
 #
