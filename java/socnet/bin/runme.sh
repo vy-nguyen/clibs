50d01ee0943c1917fdadbf2240294f40bd03e3c2
Vy Nguyen <vynguyen@bienhoa>
2017-07-03 09:38:42 -0700

    Fix up correct jar path.

--- java/socnet/bin/runme.sh
+++ java/socnet/bin/runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ !-e jetty-runner* ]; then
+if [ ! -e ./jetty-runner* ]; then
     wget http://central.maven.org/maven2/org/eclipse/jetty/jetty-runner/9.3.0.M1/jetty-runner-9.3.0.M1.jar
 fi
 
-java -jar jetty-runner-9.3.0.M1.jar --port 8080 build/libs/socnet-0.0.1-SNAPSHOT.war
+java -jar jetty-runner-9.3.0.M1.jar --port 8080 ../build/libs/socnet-0.0.1-SNAPSHOT.war
