f00f8e32b3384d83fcbbfdae14d58f2e330b2241
Vy Nguyen <vynguyen@bienhoa>
2017-11-16 08:27:39 -0800

    Try react 15.6.0 version

--- java/socnet/src/front-end/webpack/scripts.js
+++ java/socnet/src/front-end/webpack/scripts.js
@@ -4,12 +4,12 @@ var scripts = {
         "vntd-root"  : "/src/front-end/tvntd",
         "node-module": "/node_modules",
 
-        "react-mod": "/node_modules/react/react.js",
+        "react-mod": "/node_modules/react/dist/react.min.js",
         "react-lib": "/node_modules/react/lib",
         "react-dom": "/node_modules/react-dom/dist/react-dom.min.js",
         "react-dom-server": "/node_modules/react-dom/server.js",
         "react-bootstrap" : "/node_modules/react-bootstrap/dist/react-bootstrap.min.js",
-        "react-router"    : "/node_modules/react-router/umd/ReactRouter.min.js",
+        "react-router"    : "/node_modules/react-router/umd/react-router.min.js",
         "react-dropzone-component": "/node_modules/react-dropzone-component/dist/react-dropzone.js",
 
         "history/lib": "/node_modules/history/umd/History.min.js",
