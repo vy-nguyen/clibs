2e3141ceb6d92852d78945ccf0506af6a5dd045b
Vy Nguyen <vynguyen@bienhoa>
2018-02-07 00:01:52 -0800

    Use gradle wrapper to exec script.

--- java/socnet/backend.sh
+++ java/socnet/backend.sh
@@ -2,5 +2,5 @@
 
 # gradle jettyRunDebug -Dorg.eclipse.jetty.annotations.maxWait=120 $@
 
-gradle jettyRun -Dorg.eclipse.jetty.annotations.maxWait=120 $@
+./gradlew jettyRun -Dorg.eclipse.jetty.annotations.maxWait=120 $@
 
