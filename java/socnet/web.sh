69a033ed20fa4207955829b9ba33ba075c9b86d7
Vy Nguyen <vy.n.nguyen@oracle.com>
2018-10-29 11:16:29 -0700

    Add googlemap key

---
+++ java/socnet/web.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DEV_ENVIRONMENT=true
+export KEY_DB_HOST=10.1.10.11
+# export KEY_DB_HOST=localhost
+
+# gradle jettyRun -Dorg.eclipse.jetty.annotations.maxWait=180 $@
+# gradle tomcatRun $@
+
+gradle jettyRunDebug -Dorg.eclipse.jetty.annotations.maxWait=180 $@
+# ./gradlew jettyRun -Dorg.eclipse.jetty.annotations.maxWait=120 $@
+
